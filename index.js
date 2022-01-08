5576d0ce3ff1f43953185125b762bdeb7214e52f
alexvlamakis <alexvlamakis@gmail.com>
2022-01-07 17:15:11 -0700

    start off addmember function with user input

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const employees = [];
+
+function initApp() {
+    startHtml();
+    addMember();
+}
+//function to add members with user input according to messages for name, email, role, and ID
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email address",
+        name: "id"
+    }])
+    .then(function({name, role, id, email}){
+        let roleInfo = "";
+    })
